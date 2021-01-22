7388a81f9bf10c7b3788a6c826684ec3f864e0f3
NebUngV2 <71672891+NebUngV2@users.noreply.github.com>
2021-01-22 16:50:36 -0500

    Add files via upload

---
+++ game.js
@@ -0,0 +1,52 @@
+class Game {
+constructor() {
+
+}
+   getState() {
+       var gameStateRef=database.ref('gameState');
+       gameStateRef.on("value", function(data){
+           gameState=data.val();
+       })
+   }
+   update(state) {
+       database.ref('/').update({
+           gameState:state
+       })
+   }
+   async start() {
+       if(gameState===0) {
+           player = new Player()
+           var playerCountRef = await database.ref('playerCount').once("value");
+           if(playerCountRef.exists()) {
+                playerCount=playerCountRef.val()
+                player.getCount()
+           }
+           form=new Form()
+           form.display();
+       }
+   }
+   play() {
+       form.hide();
+       textSize(30);
+       text("Game Started", 120, 100);
+       Player.getPlayerInfo()
+       if(allPlayers!==undefined) {
+           var a = 120
+           for(var plr in allPlayers) {
+               if(plr==="player" +player.index) {
+                   fill("RED");
+               }
+               else {
+                  fill("Black");
+               }
+           }
+           a+=20
+           textSize(15)
+           text(allPlayers[plr].name+":"+allPlayers[plr].distance, 120, a)
+       }
+       if(keyDown(UP_ARROW)&&player.index!==null) {
+        player.index+=50
+        player.update();
+    }
+   }
+}
